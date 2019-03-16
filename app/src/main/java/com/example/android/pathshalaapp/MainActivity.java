6a9de997671ff9822c75271b3568c1cd83a2bc2b
sanketagarwal17 <sanketagarwal702@gmail.com>
2019-03-16 14:17:15 +0530

    update

--- app/src/main/java/com/example/android/pathshalaapp/MainActivity.java
+++ app/src/main/java/com/example/android/pathshalaapp/MainActivity.java
@@ -1,9 +1,5 @@
 package com.example.android.pathshalaapp;
 
-<<<<<<< HEAD
-
-=======
->>>>>>> a083ff10cb8486cf24bd9f093a92ab920878d78f
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -14,11 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        startActivity(new Intent(MainActivity.this,HomeActivity.class));
+        startActivity(new Intent(MainActivity.this, HomeActivity.class));
 
     }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> a083ff10cb8486cf24bd9f093a92ab920878d78f
+
